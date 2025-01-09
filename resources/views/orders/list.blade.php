2f88904d4a3f69ef9c9c9d70e78bd579aaeae594
ajayathilaka <ajayathilaka@schubergphilis.com>
2025-01-09 16:07:11 +0100

    display products

--- resources/views/orders/list.blade.php
+++ resources/views/orders/list.blade.php
@@ -1,1 +1,29 @@
-orders
+<x-layout>
+    <x-slot:title>
+        Top Ten Products
+    </x-slot>
+
+        <h3>Top Ten Products</h3>
+
+        <table class="table table-sm">
+            <thead>
+                <tr>
+                    <th>GTIN</th>
+                    <th>Product Name</th>
+                    <th>Count</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($topProducts as $product)
+                <tr>
+                    <td>{{ $product['gtin'] }}</td>
+                    <td>{{ $product['productName'] }}</td>
+                    <td>{{ $product['count'] }}</td>
+                    <td><a class="btn btn-link" href="">Update Stock</a></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+</x-layout>

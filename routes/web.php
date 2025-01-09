121687b2521fafd18383f1fe89cef0a844c84d6e
ajayathilaka <ajayathilaka@schubergphilis.com>
2025-01-09 12:22:21 +0100

    basic controller and view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrdersController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [OrdersController::class, 'show']);
